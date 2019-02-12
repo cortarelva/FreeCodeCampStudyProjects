05c654f1845cca1bbf0b51949cb0c90924e815d9
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-02-12 10:41:39 +0000

    new test commit

---
+++ Survey Form/JS/script.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+
+let name = document.getElementById("name");
+
+const nameValidate = () => {
+   
+if (name == " "){
+
+ alert ("insira o seu nome!");
+}
+}
+document.getElementById("button").onclick = nameValidate();
+
+
+
+function buttonAlert(){
+
+   alert("ola, ola, ola!")
+    
+}
+
+document.getElementById("button").onclick =  buttonAlert();
+
+
+}
